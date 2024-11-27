4dc51dbf5a9c9e076cd24142e2370951ca5987fe
bryanmsombra27 <bryanjm96@gmail.com>
2024-11-26 18:36:53 -0600

    working with signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { provideRouter, RouterOutlet } from '@angular/router';
+import { routes } from './app.routes';
+import { CommonModule } from '@angular/common';
+import { PropertiesPageComponent } from './signals/pages/properties-page/properties-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-new-features';
---
+++ src/app/signals/services/user-service.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { MovieDetails, User } from '../interfaces/User.interface';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserServiceService {
+  // injeccion de dependencias
+  private http = inject(HttpClient);
+  private baseUrl = 'https://reqres.in/api';
+
+  getUserById(id: number): Observable<User> {
+    return this.http
+      .get<MovieDetails>(`${this.baseUrl}/users/${id}`)
+      .pipe(map((response) => response.data));
+  }
+}
